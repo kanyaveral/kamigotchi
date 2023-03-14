86837b264d21bd466d37f6c6f0684caa18a7018b
jirach <acheron@asphodel.io>
2023-03-14 01:31:42 -0700

    chore(network): refactor boot
    primarily this moves the creation of the game config to the
    createGameConfig file in layers/network/
    
    this introduces some bugs that need to be worked through, namely the
    components not reseting upon refresh

--- packages/client/src/boot.tsx
+++ packages/client/src/boot.tsx
@@ -12,117 +12,23 @@ import {
   setComponent,
 } from '@latticexyz/recs';
 import { Time } from './utils/time';
-import { Wallet } from 'ethers';
+import { createGameConfig } from './layers/network/createGameConfig';
 
 let createNetworkLayer = createNetworkLayerImport;
 let createPhaserLayer = createPhaserLayerImport;
 let registerUIComponents = registerUIComponentsImport;
 let Engine = EngineImport;
 
+
+export async function boot() {
+  bootReact();
+  const game = await bootGame();
+  setLayers.current(game.layers as Layers);
+}
+
+// Boot the game.
 async function bootGame() {
-  const layers: Partial<Layers> = {};
-  let initialBoot = true;
-
-  async function rebootGame(): Promise<Layers> {
-    mountReact.current(false);
-
-    const params = new URLSearchParams(window.location.search);
-    let worldAddress,
-      wallet,
-      chainIdString,
-      jsonRpc,
-      wsRpc,
-      checkpointUrl,
-      initialBlockNumber;
-    let relayServiceUrl = '',
-      faucetServiceUrl = '',
-      snapshotUrl = '';
-
-    const devMode = params.get('dev') === 'true';
-
-    // LOCAL
-    if (devMode) {
-      worldAddress = params.get('worldAddress');
-      chainIdString = params.get('chainId');
-      jsonRpc = params.get('rpc') || undefined;
-      wsRpc = params.get('wsRpc') || undefined; // || (jsonRpc && jsonRpc.replace("http", "ws"));
-      checkpointUrl = params.get('checkpoint') || undefined;
-      snapshotUrl = params.get('snapshotUrl') || '';
-      let initialBlockNumberString = params.get('initialBlockNumber');
-      initialBlockNumber = initialBlockNumberString
-        ? parseInt(initialBlockNumberString)
-        : 0;
-
-      wallet = new Wallet(
-        '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
-      );
-      localStorage.setItem('burnerPrivateKey', wallet.privateKey);
-      localStorage.setItem('burnerAddress', wallet.publicKey);
-
-      // TESTNET
-    } else {
-      relayServiceUrl = "https://ecs-relay.testnet-mud-services.linfra.xyz";
-      faucetServiceUrl = "https://faucet.testnet-mud-services.linfra.xyz";
-      snapshotUrl = "https://ecs-snapshot.testnet-mud-services.linfra.xyz";
-      jsonRpc = "https://follower.testnet-chain.linfra.xyz";
-      wsRpc = "wss://follower.testnet-chain.linfra.xyz";
-      worldAddress = "0xfEF57aF100788255165c470621d19d4673e9ED91";
-      chainIdString = "4242";
-      checkpointUrl = params.get("checkpoint") || undefined;
-      // let initialBlockNumberString = params.get("initialBlockNumber") || (testnet ? "474980" : "0");
-      // initialBlockNumber = initialBlockNumberString ? parseInt(initialBlockNumberString) : 0;
-      initialBlockNumber = 0;
-
-      let privateKey = localStorage.getItem("burnerWallet")
-      wallet = privateKey ? new Wallet(privateKey) : Wallet.createRandom();
-      localStorage.setItem("burnerWallet", wallet.privateKey);
-      localStorage.setItem("burnerWalletAddress", wallet.publicKey);
-    }
-
-    let networkLayerConfig;
-    if (worldAddress && wallet && chainIdString && jsonRpc) {
-      networkLayerConfig = {
-        worldAddress,
-        privateKey: wallet.privateKey,
-        chainId: parseInt(chainIdString),
-        jsonRpc,
-        wsRpc,
-        checkpointUrl,
-        devMode,
-        initialBlockNumber,
-        faucetServiceUrl,
-        relayServiceUrl,
-        snapshotUrl,
-      };
-    }
-
-    if (!networkLayerConfig) throw new Error('Invalid config');
-
-    if (!layers.network)
-      layers.network = await createNetworkLayer(networkLayerConfig);
-    if (!layers.phaser) layers.phaser = await createPhaserLayer(layers.network);
-
-    Time.time.setPacemaker((setTimestamp) => {
-      layers.phaser?.game.events.on('poststep', (time: number) => {
-        setTimestamp(time);
-      });
-    });
-
-    if (document.querySelectorAll('#phaser-game canvas').length > 1) {
-      console.log('Detected two canvas elements, full reload');
-    }
-
-    if (initialBoot) {
-      initialBoot = false;
-      layers.network.startSync();
-    }
-
-    mountReact.current(true);
-
-    return layers as Layers;
-  }
-
-  await rebootGame();
+  const layers = await rebootGame(true);
 
   const ecs = {
     setComponent,
@@ -135,17 +41,57 @@ async function bootGame() {
   (window as any).time = Time.time;
 
   console.log('booted');
-
   return { layers, ecs };
 }
 
+// Reboots the game. Syncs data if initial boot.
+// TODO: avoid syncing the data every time. Cache the data and save the most recent block seen.
+async function rebootGame(initialBoot: boolean): Promise<Layers> {
+  mountReact.current(false);
+  const layers: Partial<Layers> = {};
+
+  // create our game config with network details
+  const gameConfig = createGameConfig();
+  if (!gameConfig) throw new Error('Invalid config');
+
+  // set the layers of the game
+  if (!layers.network) layers.network = await createNetworkLayer(gameConfig);
+  if (!layers.phaser) layers.phaser = await createPhaserLayer(layers.network);
+
+  // wot is going on here
+  Time.time.setPacemaker((setTimestamp) => {
+    layers.phaser?.game.events.on('poststep', (time: number) => {
+      setTimestamp(time);
+    });
+  });
+
+  // failsafe for buggy load
+  if (document.querySelectorAll('#phaser-game canvas').length > 1) {
+    console.log('Detected two canvas elements, full reload');
+  }
+
+  // start syncing the network
+  if (initialBoot) {
+    initialBoot = false;
+    layers.network.startSync();
+  }
+
+  mountReact.current(true);
+
+  return layers as Layers;
+}
+
+// what can we do with this?
 const mountReact: { current: (mount: boolean) => void } = {
   current: () => void 0,
 };
+
+// what can we do with this?
 const setLayers: { current: (layers: Layers) => void } = {
   current: () => void 0,
 };
 
+// TODO: wait until the world is indexed before registering our data dependent UI components
 function bootReact() {
   const rootElement = document.getElementById('react-root');
   if (!rootElement) return console.warn('React root not found');
@@ -158,10 +104,4 @@ function bootReact() {
 
   renderEngine();
   registerUIComponents();
-}
-
-export async function boot() {
-  bootReact();
-  const game = await bootGame();
-  setLayers.current(game.layers as Layers);
 }
---
+++ packages/client/src/layers/network/createGameConfig.ts
@@ -0,0 +1,80 @@
+import { Wallet } from 'ethers';
+import { GameConfig } from './config';
+
+// Populate the network config based on url params
+export function createGameConfig(): GameConfig | undefined {
+  let config: GameConfig = <GameConfig>{};
+
+  const params = new URLSearchParams(window.location.search);
+  const devMode = params.get('dev') === 'true';
+  config = (devMode) ? createGameConfigLocal(params) : createGameConfigLattice();
+  console.log('config', config);
+
+  if (
+    config.privateKey
+    && config.worldAddress
+    && config.jsonRpc
+    && config.chainId
+  ) {
+    return config;
+  }
+}
+
+// Get the network config of a local deployment based on url params
+function createGameConfigLocal(params: URLSearchParams): GameConfig {
+  let config: GameConfig = <GameConfig>{};
+
+  // EOAs
+  const wallet = new Wallet(
+    '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+  );
+  localStorage.setItem('burnerPrivateKey', wallet.privateKey);
+  localStorage.setItem('burnerAddress', wallet.publicKey);
+  config.privateKey = wallet.privateKey;
+
+  // RPCs
+  const jsonRpc = params.get('rpc') || "http://localhost:8545";
+  const wsRpc = params.get('wsRpc') || (jsonRpc && jsonRpc.replace("http", "ws"));
+  config.jsonRpc = jsonRpc;
+  config.wsRpc = wsRpc;
+
+  // urls
+  config.checkpointUrl = params.get('checkpoint') || undefined;
+  config.snapshotUrl = params.get('snapshotUrl') || undefined;;
+
+  // chainId
+  const chainIdString = params.get('chainId') || '31337';
+  config.chainId = parseInt(chainIdString);
+
+  // world
+  config.worldAddress = params.get('worldAddress') || '';
+
+  // block number
+  let initialBlockNumberString = params.get('initialBlockNumber') || '0';
+  config.initialBlockNumber = parseInt(initialBlockNumberString);
+
+  return config;
+}
+
+// Get the network config of a deployment to Lattice's mudChain testnet
+function createGameConfigLattice(): GameConfig {
+  // setting up local burner
+  let privateKey = localStorage.getItem("burnerAddress");
+  const wallet = privateKey ? new Wallet(privateKey) : Wallet.createRandom();
+  localStorage.setItem("burnerPrivateKey", wallet.privateKey);
+  localStorage.setItem("burnerAddress", wallet.publicKey);
+
+  let config: GameConfig = <GameConfig>{
+    privateKey: wallet.privateKey,
+    jsonRpc: "https://follower.testnet-chain.linfra.xyz",
+    wsRpc: "wss://follower.testnet-chain.linfra.xyz",
+    faucetServiceUrl: "https://faucet.testnet-mud-services.linfra.xyz",
+    relayServiceUrl: "https://ecs-relay.testnet-mud-services.linfra.xyz",
+    snapshotUrl: "https://ecs-snapshot.testnet-mud-services.linfra.xyz",
+    // checkpointUrl: undefined,
+    chainId: 4242,
+    worldAddress: "0xfEF57aF100788255165c470621d19d4673e9ED91", // this is the asphodel playtest
+    initialBlockNumber: 0,
+  };
+  return config;
+}
