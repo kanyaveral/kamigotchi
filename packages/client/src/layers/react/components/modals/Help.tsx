30fed53aa417ccc9291226a918bbcf33ee747dae
Darkvendor <129950379+Darkvendor@users.noreply.github.com>
2023-05-17 17:47:55 +0200

    help me ^^ (#139)

--- packages/client/src/layers/react/components/modals/Help.tsx
+++ packages/client/src/layers/react/components/modals/Help.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { map, merge } from 'rxjs';
+import React, { useState } from 'react';
+import { of } from 'rxjs';
 import styled from 'styled-components';
-import { EntityIndex, Has, HasValue, getComponentValue, runQuery } from '@latticexyz/recs';
-import * as mqtt from 'mqtt';
-
-import mutedSoundImage from '../../../../assets/images/sound_muted_native.png';
-import soundImage from '../../../../assets/images/sound_native.png';
-import { dataStore } from 'layers/react/store/createStore';
-
 import { ModalWrapperFull } from 'layers/react/components/library/ModalWrapper';
 import { registerUIComponent } from 'layers/react/engine/store';
 import 'layers/react/styles/font.css';
+
+export enum HelpComponentState {
+  HOME_PAGE,
+  KAMI_STATS,
+  KAMI_INFO,
+  OVERALL_HELP,
+}
 
 export function registerHelpModal() {
   registerUIComponent(
@@ -22,59 +22,102 @@ export function registerHelpModal() {
       rowEnd: 62,
     },
 
-    (layers) => {
-      const {
-        network: {
-          components: { IsAccount, Name },
-        },
-      } = layers;
-
-      return merge(IsAccount.update$, Name.update$).pipe(
-        map(() => {
-          return {
-            chatName: 'hi',
-          };
-        })
-      );
-    },
+    (layers) => of(layers),
 
     () => {
-      const [volumeSliderVisibility, setvolumeSliderVisibility] = useState(false);
+      let helpContent = null;
+      const [helpState, setHelpState] = useState<HelpComponentState>(HelpComponentState.HOME_PAGE);
+      function handleLinkClick(state: HelpComponentState) {
+        setHelpState(state);
+      }
 
-      const {
-        sound: { volume },
-        setSoundState,
-      } = dataStore();
-      const muted = volume == 0;
-
-      const setVolumeRate = (e: any) => {
-        const soundVolume = e.target.value as number;
-
-        setSoundState({ volume: soundVolume });
-      };
-
-      const toggleSound = () => {
-        muted ? setSoundState({ volume: 0.5 }) : setSoundState({ volume: 0 });
-      };
-
-      const handleVolumeSliderVisibility = (isVisible: boolean) => {
-        setvolumeSliderVisibility(isVisible);
-      };
+      switch (helpState) {
+        case HelpComponentState.HOME_PAGE:
+          helpContent = (
+            <div>
+              <Header>Help/Docs</Header>
+              <div style={{ display: 'flex', flexDirection: 'column' }}>
+                <Link onClick={() => handleLinkClick(HelpComponentState.KAMI_STATS)}>
+                  Kami Stats
+                </Link>
+                <Link onClick={() => handleLinkClick(HelpComponentState.KAMI_INFO)}>Kami Info</Link>
+                <Link onClick={() => handleLinkClick(HelpComponentState.OVERALL_HELP)}>
+                  Overall Help
+                </Link>
+              </div>
+            </div>
+          );
+          break;
+        case HelpComponentState.KAMI_STATS:
+          helpContent = (
+            <div>
+              <Button onClick={() => handleLinkClick(HelpComponentState.HOME_PAGE)}>
+                Home Page
+              </Button>
+              <Header>Kami Stats Help</Header>
+              {/* Kami Stats specific content */}
+            </div>
+          );
+          break;
+        case HelpComponentState.KAMI_INFO:
+          helpContent = (
+            <div>
+              <Button onClick={() => handleLinkClick(HelpComponentState.HOME_PAGE)}>
+                Home Page
+              </Button>
+              <Header>Kami Info Help</Header>
+              {/* Kami Info specific content */}
+            </div>
+          );
+          break;
+        case HelpComponentState.OVERALL_HELP:
+          helpContent = (
+            <div>
+              <Button onClick={() => handleLinkClick(HelpComponentState.HOME_PAGE)}>
+                Home Page
+              </Button>
+              <Header>Overall Help</Header>
+              {/* Overall Help specific content */}
+            </div>
+          );
+          break;
+        default:
+          helpContent = (
+            <div>
+              <Button onClick={() => handleLinkClick(HelpComponentState.HOME_PAGE)}>
+                Home Page
+              </Button>
+              <Header>Help/Docs</Header>
+              {/* Default content */}
+            </div>
+          );
+          break;
+      }
 
       return (
         <ModalWrapperFull divName='help' id='help_modal'>
-        <Header> Help/Docs </Header>
-          <Description>
-            Kamigotchi World exists entirely on-chain.
-            <br />
-            <br />
-            It currently uses a burner wallet in your LocalStorage for testing.
-          </Description>
+          {helpContent}
         </ModalWrapperFull>
       );
     }
   );
 }
+
+const Button = styled.button`
+  background-color: #222;
+  color: #fff;
+  padding: 10px 20px;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+`;
+
+// Styled link component
+const Link = styled.a`
+  color: #222;
+  text-decoration: underline;
+  cursor: pointer;
+`;
 
 const rangeInputStyle = {
   width: '55px',
