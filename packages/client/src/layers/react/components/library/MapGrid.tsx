83072d4a644e31f3616dc725169d59e40752f1c9
Lethe <102979407+asphodellethe@users.noreply.github.com>
2023-05-26 11:16:09 +0100

    fixed map import

--- packages/client/src/layers/react/components/library/MapGrid.tsx
+++ packages/client/src/layers/react/components/library/MapGrid.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { location, road, coreSprites, water } from 'assets/newmap';
+import { location, road, coreSprites, water } from 'assets/map';
 
 interface MapProps {
   highlightedRoom?: number;
