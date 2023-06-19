4b8f03c17dceacebfe08e9bc2ea1db47a01b8586
jirach <acheron@asphodel.io>
2023-06-19 03:22:51 -0400

    new mint test (placeholder)

---
+++ packages/contracts/src/test/systems/ERC721Mint.t.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "test/utils/SetupTemplate.s.sol";
+
+// Things we want to test for Mint (probably through a dedicated test)
+// - whitelist minting (only one allowed)
+// - single paid minting (multiple times over)
+// - batch paid minting (multiple times over)
+//
+// For each of the above cases we want to test
+// - proper account allocation of kami
+// - proper detraction of funds
+// - proper respect for minting limits
+//
+// We'll also want to check for proper kami initial values
+// - IsPetComponent
+// - IndexPetComponent
+// - setAccount
+// - setMediaURI
+// - setState
+// - setExperience
+//
+// Best not to rely on SetupTemplate Functions for this. There, we'll want to
+// enable free mints (through config) for ease of use in testing.
+
+contract ERC721MintTest is SetupTemplate {
+  function setUp() public override {
+    super.setUp();
+
+    _setConfig("MINT_PRICE", 1e18);
+    _registerAccount(0);
+    _registerAccount(1);
+    _registerAccount(2);
+  }
+}
