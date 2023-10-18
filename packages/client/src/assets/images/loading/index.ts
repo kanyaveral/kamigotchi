f5e61bbd36f80fb6bd2c32fd60e8489d7fe24946
jirach <109483360+JirAcheron@users.noreply.github.com>
2023-10-18 05:53:27 -0400

    cycle bootscreen (#371)

---
+++ packages/client/src/assets/images/loading/index.ts
@@ -0,0 +1,19 @@
+import jackkahunalaguna from './jackkahunalaguna_black.png';
+import killyou from './killyou_black.png';
+import longrng from './longrng_black.png';
+import mazzy from './mazzy_black.png';
+import netorarecrypto from './netorarecrypto_black.png';
+import opnpc from './opnpc_black.png';
+import qko from './qko_black.png';
+import simsrichard from './simsrichard_black.png';
+
+export const loadingScreens = [
+  jackkahunalaguna,
+  killyou,
+  longrng,
+  mazzy,
+  netorarecrypto,
+  opnpc,
+  qko,
+  simsrichard
+];
--- packages/client/src/layers/react/engine/components/BootScreen.tsx
+++ packages/client/src/layers/react/engine/components/BootScreen.tsx
@@ -1,18 +1,30 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
-import original from 'assets/images/loading/original.png'
+import { loadingScreens } from "assets/images/loading";
 
 export const BootScreen: React.FC<{}> = ({ children }) => {
+  const [image, setImage] = useState();
 
-  // TODO: on load, select a random banner to use
+  // set the banner on startup and on a 5 second interval
   useEffect(() => {
-
+    refreshBanner();
+    const timerId = setInterval(refreshBanner, 5000);
+    return function cleanup() {
+      clearInterval(timerId);
+    };
   }, []);
+
+  // swaps the banner image
+  const refreshBanner = () => {
+    const rand = Math.floor(Math.random() * loadingScreens.length)
+    const randomBanner = loadingScreens[rand];
+    setImage(randomBanner);
+  };
 
   return (
     <Container>
-      <Image src={original} />
-      <div>{children || <>&nbsp;</>}</div>
+      <Image src={image} />
+      <Text>{children || <>&nbsp</>}</Text>
     </Container>
   );
 };
@@ -22,22 +34,27 @@ const Container = styled.div`
   height: 100%;
   position: absolute;
   background-color: #000;
-  display: grid;
   align-content: center;
   align-items: center;
   justify-content: center;
   justify-items: center;
   transition: all 2s ease;
-  grid-gap: 50px;
-  z-index: 100;
   pointer-events: all;
-
-  div {
-    font-family: "Space Grotesk", sans-serif;
-  }
+  z-index: 100;
 `;
 
 const Image = styled.img`
   transition: all 2s ease;
-  width: 800px;
+  width: 100%;
+`;
+
+const Text = styled.div`
+  color: #fff;
+  position: absolute;
+  bottom: 15vw;
+  width: 100%;
+
+  text-align: center;
+  font-family: "Space Grotesk", sans-serif;
+  font-size: 20px;
 `;
