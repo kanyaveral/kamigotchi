b55a1db2a1f5987f802653f2bfbfda45ad932f61
jirach <109483360+JirAcheron@users.noreply.github.com>
2023-12-19 04:45:56 -0500

    Fix Farm20 Bridge Tests (#473)
    Fixes and cleans up musu bridge tests for Withdraw and Permissions

--- packages/contracts/src/test/systems/Farm20.t.sol
+++ packages/contracts/src/test/systems/Farm20.t.sol
@@ -5,24 +5,8 @@ import "test/utils/SetupTemplate.s.sol";
 
 contract Farm20Test is SetupTemplate {
   Farm20 token;
-  uint256 constant MAX_INT = 2 ** 256 - 1;
-  uint256 public minDelay;
-
-  // converts ERC20 decimals (18) to game decimals (0)
-  function _tokenToGameDP(uint256 amount) internal pure returns (uint256) {
-    return amount / 10 ** 18;
-  }
-
-  // converts game decimals (0) to ERC20 decimals (18)
-  function _gameToTokenDP(uint256 amount) internal pure returns (uint256) {
-    return amount * 10 ** 18;
-  }
-
-  function _uncheckedAdd(uint256 a, uint256 b) internal pure returns (uint256) {
-    unchecked {
-      return a + b;
-    }
-  }
+  uint constant MAX_INT = 2 ** 256 - 1;
+  uint public minDelay;
 
   function setUp() public override {
     super.setUp();
@@ -30,105 +14,120 @@ contract Farm20Test is SetupTemplate {
 
     _createRoom("testRoom1", 1, 4, 12, 0);
     _createRoom("testRoom4", 4, 1, 12, 0);
-    _createRoom("testRoom4", 12, 1, 4, 0);
+    _createRoom("testRoom12", 12, 1, 4, 0);
 
     _registerAccount(0);
     _registerAccount(1);
+    _moveAccount(0, 12); // bridging restricted to room 12
+    _moveAccount(1, 12); // bridging restricted to room 12
 
     minDelay = _Farm20WithdrawSystem.getMinDelay();
   }
 
+  /////////////////
+  // HELPER FUNCS
+
+  // converts ERC20 decimals (18) to game decimals (0)
+  function _tokenToGameDP(uint amount) internal pure returns (uint) {
+    return amount / 10 ** 18;
+  }
+
+  // converts game decimals (0) to ERC20 decimals (18)
+  function _gameToTokenDP(uint amount) internal pure returns (uint) {
+    return amount * 10 ** 18;
+  }
+
+  function _uncheckedAdd(uint a, uint b) internal pure returns (uint) {
+    unchecked {
+      return a + b;
+    }
+  }
+
   // tests that correct amounts are withdrawn
+  // TODO: test multiple layered withdraws scheduled
   // TODO: check that the tested account is the only one with a coin value
-  function testWithdraw(uint256 startingBalance, uint256 amt, uint256 delay) public {
-    vm.assume(startingBalance < _tokenToGameDP(MAX_INT));
-    vm.assume(amt < _tokenToGameDP(MAX_INT));
-    vm.assume(_uncheckedAdd(_currTime, delay) > _currTime);
-    _fundAccount(0, startingBalance);
-    _moveAccount(0, 12); // bridging restricted to room 12
+  function testWithdrawSingle(uint startBal, uint amt, uint delay) public {
+    vm.assume(startBal < _tokenToGameDP(MAX_INT)); // starting balance is lower than max int
+    vm.assume(amt < _tokenToGameDP(MAX_INT)); // withdraw amount is lower than max int
+    vm.assume(_uncheckedAdd(_currTime, delay) > _currTime); // prevent time wrap-arounds
+    _fundAccount(0, startBal);
 
     vm.startPrank(_getOwner(0));
 
-    // withdraw the amt from the game
+    // check constraints on scheduling a withdraw
     if (amt == 0) {
       vm.expectRevert("Farm20Withdraw: amt must be > 0");
       _Farm20WithdrawSystem.scheduleWithdraw(amt);
-    } else if (delay <= minDelay) {
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt);
-      _fastForward(delay);
-      vm.expectRevert("operation not ready");
-      _Farm20WithdrawSystem.executeWithdraw(id);
-    } else {
-      if (amt > startingBalance) {
-        vm.expectRevert("Coin: insufficient balance");
-        _Farm20WithdrawSystem.scheduleWithdraw(amt);
-        return;
-      }
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt);
-      // check operation struct
-      (address target, uint256 value, uint256 salt) = LibTimelock.getTimelock(components, id);
-      assertEq(target, _getOwner(0));
-      assertEq(value, amt);
-      assertEq(salt, block.timestamp);
-      // roll
-      _fastForward(delay);
-      if (amt > startingBalance) {
-        vm.expectRevert("Coin: insufficient balance");
-        _Farm20WithdrawSystem.executeWithdraw(id);
-      } else {
-        _Farm20WithdrawSystem.executeWithdraw(id);
-
-        // check the user has the correct balances in and out of the game
-        uint256 internalBalance = _CoinComponent.getValue(_getAccount(0));
-        assertEq(internalBalance, startingBalance - amt);
-
-        uint256 externalBalance = token.balanceOf(_getOwner(0));
-        assertEq(_tokenToGameDP(externalBalance), amt);
-
-        // check that the total supply is the balance out of the world
-        assertEq(_tokenToGameDP(token.totalSupply()), amt);
-      }
+      return;
     }
+    if (amt > startBal) {
+      vm.expectRevert("Coin: insufficient balance");
+      _Farm20WithdrawSystem.scheduleWithdraw(amt);
+      return;
+    }
+
+    // schedule a withdraw, deconstruct timelock operation and fastforward by the delay
+    uint withdrawID = _Farm20WithdrawSystem.scheduleWithdraw(amt);
+    (address target, uint value, uint salt) = LibTimelock.getTimelock(components, withdrawID);
+    assertEq(target, _getOwner(0));
+    assertEq(value, amt);
+    assertEq(salt, block.timestamp);
+    _fastForward(delay);
+
+    // check constraints on executing a withdraw
+    if (delay <= minDelay) {
+      vm.expectRevert("operation not ready");
+      _Farm20WithdrawSystem.executeWithdraw(withdrawID);
+      return;
+    }
+
+    // execute withdraw and confirm correct internal/external/total balances
+    _Farm20WithdrawSystem.executeWithdraw(withdrawID);
+    uint internalBalance = _CoinComponent.getValue(_getAccount(0));
+    uint externalBalance = token.balanceOf(_getOwner(0));
+    assertEq(internalBalance, startBal - amt);
+    assertEq(_tokenToGameDP(externalBalance), amt);
+    assertEq(_tokenToGameDP(token.totalSupply()), amt); // total contract supply
 
     vm.stopPrank();
   }
 
   // tests that correct amounts are deposited
-  function testDeposit(uint256 startingBalance, uint256 amt) public {
-    vm.assume(startingBalance > 0);
-    vm.assume(startingBalance < _tokenToGameDP(MAX_INT));
-    vm.assume(amt < _tokenToGameDP(MAX_INT));
-    _fundAccount(0, startingBalance);
-    _moveAccount(0, 12); // bridging restricted to room 12
+  function testDeposit(uint startBal, uint amt) public {
+    vm.assume(startBal < _tokenToGameDP(MAX_INT)); // starting balance is lower than max int
+    vm.assume(amt < _tokenToGameDP(MAX_INT)); // deposit amount is lower than max int
+    _fundAccount(0, startBal);
 
     vm.startPrank(_getOwner(0));
 
     // pull out the full balance of funds
-    uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(startingBalance);
-    _fastForward(minDelay + 1);
-    _Farm20WithdrawSystem.executeWithdraw(id);
+    if (startBal > 0) {
+      uint withdrawID = _Farm20WithdrawSystem.scheduleWithdraw(startBal);
+      _fastForward(minDelay + 1);
+      _Farm20WithdrawSystem.executeWithdraw(withdrawID);
+    }
 
-    // deposit the amt back into the game
+    // check deposit constraints
     if (amt == 0) {
       vm.expectRevert("Farm20Deposit: amt must be > 0");
       _Farm20DepositSystem.executeTyped(amt);
-    } else if (amt > startingBalance) {
-      // vm.expectRevert("Arithmetic over/underflow"); // cannot get this matching, maybe insert our own error into the flow
+      return;
+    }
+    if (amt > startBal) {
+      // cannot get this matching, maybe insert our own error into the flow
+      // vm.expectRevert("Arithmetic over/underflow");
       vm.expectRevert();
       _Farm20DepositSystem.executeTyped(amt);
-    } else {
-      _Farm20DepositSystem.executeTyped(amt);
-
-      // check the user has the correct balances in and out of the game
-      uint256 internalBalance = _CoinComponent.getValue(_getAccount(0));
-      assertEq(internalBalance, amt);
-
-      uint256 externalBalance = token.balanceOf(_getOwner(0));
-      assertEq(_tokenToGameDP(externalBalance), startingBalance - amt);
-
-      // check that the total supply is the balance expected out of the world
-      assertEq(_tokenToGameDP(token.totalSupply()), startingBalance - amt);
+      return;
     }
+
+    // check the user has the correct balances in and out of the game
+    _Farm20DepositSystem.executeTyped(amt);
+    uint internalBalance = _CoinComponent.getValue(_getAccount(0));
+    uint externalBalance = token.balanceOf(_getOwner(0));
+    assertEq(internalBalance, amt);
+    assertEq(_tokenToGameDP(externalBalance), startBal - amt);
+    assertEq(_tokenToGameDP(token.totalSupply()), startBal - amt); // total contract supply
 
     vm.stopPrank();
   }
@@ -136,15 +135,14 @@ contract Farm20Test is SetupTemplate {
   // fund one account, have them withdraw, transfer the tokens to an address without an account.
   // when the address without and account attempt to deposit funds, this should fail.
   // TODO: implement this
-  function testInvalidDeposit(uint256 amt) public {
+  function testInvalidDeposit(uint amt) public {
     vm.assume(amt > 0);
     vm.assume(amt < _tokenToGameDP(MAX_INT));
     _fundAccount(0, amt);
-    _moveAccount(0, 12); // bridging restricted to room 12
 
     // withdraw full amount
     vm.prank(_getOwner(0));
-    uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt);
+    uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt);
     _fastForward(minDelay + 1);
     vm.prank(_getOwner(0));
     _Farm20WithdrawSystem.executeWithdraw(id);
@@ -162,10 +160,10 @@ contract Farm20Test is SetupTemplate {
   // run multiple deposits and withdrawals interwoven between multiple accounts
   // test that all balances are as expected at the end
   function testMultiple(
-    uint256 startBal0,
-    uint256 startBal1,
-    uint256 amt0,
-    uint256 amt1,
+    uint startBal0,
+    uint startBal1,
+    uint amt0,
+    uint amt1,
     bool withDeposit
   ) public {
     vm.assume(startBal0 > 0);
@@ -178,38 +176,34 @@ contract Farm20Test is SetupTemplate {
     _fundAccount(0, startBal0);
     _fundAccount(1, startBal1);
 
-    // bridging restricted to room 12
-    _moveAccount(0, 12);
-    _moveAccount(1, 12);
-
     // withdraw from account 0
     vm.startPrank(_getOwner(0));
     if (amt0 == 0) {
       vm.expectRevert("Farm20Withdraw: amt must be > 0");
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
 
       return;
     } else if (amt0 > startBal0) {
       vm.expectRevert("Coin: insufficient balance");
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
 
       return;
     } else {
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt0);
       _fastForward(minDelay + 1);
       _Farm20WithdrawSystem.executeWithdraw(id);
 
       // check the user has the correct balances in and out of the game
-      uint256 internalBalance = _CoinComponent.getValue(_getAccount(0));
+      uint internalBalance = _CoinComponent.getValue(_getAccount(0));
       assertEq(internalBalance, startBal0 - amt0);
 
-      uint256 externalBalance = token.balanceOf(_getOwner(0));
+      uint externalBalance = token.balanceOf(_getOwner(0));
       assertEq(_tokenToGameDP(externalBalance), amt0);
 
-      uint256 internalBalance1 = _CoinComponent.getValue(_getAccount(1));
+      uint internalBalance1 = _CoinComponent.getValue(_getAccount(1));
       assertEq(internalBalance1, startBal1);
 
-      uint256 externalBalance1 = token.balanceOf(_getOwner(1));
+      uint externalBalance1 = token.balanceOf(_getOwner(1));
       assertEq(_tokenToGameDP(externalBalance1), 0);
 
       // check that the total supply is the balance out of the world
@@ -227,46 +221,46 @@ contract Farm20Test is SetupTemplate {
     vm.startPrank(_getOwner(1));
     if (amt1 == 0) {
       vm.expectRevert("Farm20Withdraw: amt must be > 0");
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
     } else if (amt1 > startBal1) {
       vm.expectRevert("Coin: insufficient balance");
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
     } else {
-      uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
+      uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt1);
       _fastForward(minDelay + 1);
       _Farm20WithdrawSystem.executeWithdraw(id);
 
       // check the user has the correct balances in and out of the game
       if (withDeposit) {
-        uint256 internalBalance = _CoinComponent.getValue(_getAccount(0));
+        uint internalBalance = _CoinComponent.getValue(_getAccount(0));
         assertEq(internalBalance, startBal0);
 
-        uint256 externalBalance = token.balanceOf(_getOwner(0));
+        uint externalBalance = token.balanceOf(_getOwner(0));
         assertEq(_tokenToGameDP(externalBalance), 0);
 
         // check that the total supply is the balance out of the world
         assertEq(_tokenToGameDP(token.totalSupply()), amt1);
       } else {
-        uint256 internalBalance = _CoinComponent.getValue(_getAccount(0));
+        uint internalBalance = _CoinComponent.getValue(_getAccount(0));
         assertEq(internalBalance, startBal0 - amt0);
 
-        uint256 externalBalance = token.balanceOf(_getOwner(0));
+        uint externalBalance = token.balanceOf(_getOwner(0));
         assertEq(_tokenToGameDP(externalBalance), amt0);
 
         // check that the total supply is the balance out of the world
         assertEq(_tokenToGameDP(token.totalSupply()), amt0 + amt1);
       }
 
-      uint256 internalBalance1 = _CoinComponent.getValue(_getAccount(1));
+      uint internalBalance1 = _CoinComponent.getValue(_getAccount(1));
       assertEq(internalBalance1, startBal1 - amt1);
 
-      uint256 externalBalance1 = token.balanceOf(_getOwner(1));
+      uint externalBalance1 = token.balanceOf(_getOwner(1));
       assertEq(_tokenToGameDP(externalBalance1), amt1);
     }
   }
 
   // test that proper balances are reflected after transfering from one address to another
-  function testTransfer(uint256 startBal0, uint256 startBal1, uint256 amt) public {
+  function testTransfer(uint startBal0, uint startBal1, uint amt) public {
     vm.assume(startBal0 > 0);
     vm.assume(startBal1 > 0);
     vm.assume(startBal0 < _tokenToGameDP(MAX_INT));
@@ -276,12 +270,8 @@ contract Farm20Test is SetupTemplate {
     _fundAccount(0, startBal0);
     _fundAccount(1, startBal1);
 
-    // bridging restricted to room 12
-    _moveAccount(0, 12);
-    _moveAccount(1, 12);
-
     vm.prank(_getOwner(0));
-    uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(startBal0);
+    uint id = _Farm20WithdrawSystem.scheduleWithdraw(startBal0);
     _fastForward(minDelay + 1);
     vm.prank(_getOwner(0));
     _Farm20WithdrawSystem.executeWithdraw(id);
@@ -316,20 +306,17 @@ contract Farm20Test is SetupTemplate {
   }
 
   function testTimelock(
-    uint256 delay,
+    uint delay,
     bool cancelled,
     bool blacklistedBefore,
     bool blacklistedAfter
   ) public {
     vm.assume(_uncheckedAdd(_currTime, delay) >= _currTime);
 
-    uint256 amt = 100;
+    uint amt = 100;
     _fundAccount(0, amt);
 
-    // bridging restricted to room 12
-    _moveAccount(0, 12);
-
-    uint256 id;
+    uint id;
     if (blacklistedBefore) {
       vm.prank(deployer);
       _Farm20WithdrawSystem.blacklist(_getOwner(0));
@@ -363,59 +350,54 @@ contract Farm20Test is SetupTemplate {
   }
 
   function testBridgeRoles() public {
-    // bridging restricted to room 12
-    _moveAccount(0, 12);
-    _moveAccount(1, 12);
-
     assertTrue(_Farm20WithdrawSystem.isAdmin(deployer));
-    assertTrue(!_Farm20WithdrawSystem.isAdmin(_getOwner(0)));
 
-    vm.prank(_getOwner(1));
-    uint256 id = _Farm20WithdrawSystem.scheduleWithdraw(100);
+    // ensure random players cannot update bridge parameters
+    for (uint i; i < _owners.length - 1; i++) {
+      assertTrue(!_Farm20WithdrawSystem.isAdmin(_getOwner(i)));
 
+      vm.startPrank(_getOwner(i));
+      vm.expectRevert();
+      _Farm20WithdrawSystem.updateMinDelay(0);
+      vm.expectRevert();
+      _Farm20WithdrawSystem.blacklist(address(0));
+      vm.expectRevert();
+      _Farm20WithdrawSystem.unblacklist(address(0));
+      vm.expectRevert();
+      _Farm20WithdrawSystem.updateAdmin(address(0), true);
+      vm.stopPrank();
+    }
+
+    // schedule a withdraw with our test account
+    uint amt = 100;
+    _fundAccount(0, amt);
     vm.prank(_getOwner(0));
-    vm.expectRevert("not authorized");
-    _Farm20WithdrawSystem.cancelWithdraw(id);
+    uint id = _Farm20WithdrawSystem.scheduleWithdraw(amt);
 
-    vm.prank(_getOwner(0));
-    vm.expectRevert();
-    _Farm20WithdrawSystem.updateMinDelay(0);
+    // ensure random players cannot cancel their withdraw
+    for (uint i = 1; i < _owners.length - 1; i++) {
+      vm.prank(_getOwner(i));
+      vm.expectRevert("not authorized");
+      _Farm20WithdrawSystem.cancelWithdraw(id);
+    }
 
-    vm.prank(_getOwner(0));
-    vm.expectRevert();
-    _Farm20WithdrawSystem.blacklist(address(0));
-
-    vm.prank(_getOwner(0));
-    vm.expectRevert();
-    _Farm20WithdrawSystem.unblacklist(address(0));
-
-    vm.prank(_getOwner(0));
-    vm.expectRevert();
-    _Farm20WithdrawSystem.updateAdmin(address(0), true);
-
+    // promote player 1 to admin
     vm.prank(deployer);
-    _Farm20WithdrawSystem.updateAdmin(_getOwner(0), true);
-    assertTrue(_Farm20WithdrawSystem.isAdmin(_getOwner(0)));
+    _Farm20WithdrawSystem.updateAdmin(_getOwner(1), true);
+    assertTrue(_Farm20WithdrawSystem.isAdmin(_getOwner(1)));
 
-    vm.startPrank(_getOwner(0));
+    // check that player 1 has admin permissions now
+    vm.startPrank(_getOwner(1));
     _Farm20WithdrawSystem.blacklist(address(0));
     _Farm20WithdrawSystem.unblacklist(address(0));
+    _Farm20WithdrawSystem.cancelWithdraw(id);
+
+    // check that player 1 does not have owner permissions
     vm.expectRevert();
     _Farm20WithdrawSystem.updateAdmin(address(0), true);
     vm.expectRevert();
     _Farm20WithdrawSystem.updateMinDelay(0);
+
     vm.stopPrank();
-
-    // bridging restricted to room 12
-    _moveAccount(0, 12);
-    _moveAccount(1, 12);
-
-    vm.prank(_getOwner(1));
-    _Farm20WithdrawSystem.cancelWithdraw(id);
-
-    vm.prank(_getOwner(1));
-    id = _Farm20WithdrawSystem.scheduleWithdraw(id);
-    vm.prank(_getOwner(0));
-    _Farm20WithdrawSystem.cancelWithdraw(id);
   }
 }
