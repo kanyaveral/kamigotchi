e3428715362f0fa8f139be4d1cb52141797c5bc0
jirach <109483360+JirAcheron@users.noreply.github.com>
2024-01-18 05:45:49 -0500

    tune down sounds (#500)
    * lower sound fx volume
    
    * reorganize sounds

---
+++ packages/client/src/assets/sound/fx/interaction/index.ts
@@ -0,0 +1,21 @@
+import success from './bubble_success.mp3';
+import click from './click.wav';
+import dice from './dice.mp3';
+import echo from './echo.mp3';
+import phase from './phase.mp3';
+import scribble from './scribble.mp3';
+import teleport from './teleport.mp3';
+import trade from './trade.wav';
+import vend from './vend.mp3';
+
+export const InteractionFX = {
+  success,
+  click,
+  dice,
+  echo,
+  phase,
+  scribble,
+  teleport,
+  trade,
+  vend,
+};
--- packages/client/src/utils/sounds.ts
+++ packages/client/src/utils/sounds.ts
@@ -1,33 +1,27 @@
-import bubble from 'assets/sound/fx/bubble_success.mp3';
-import click from 'assets/sound/fx/mouseclick.wav';
-import scribble from 'assets/sound/fx/scribbling.mp3';
-import vending from 'assets/sound/fx/vending_machine.mp3';
+import { InteractionFX } from 'assets/sound/fx/interaction';
 import { useSound } from 'layers/react/store/sound';
 
 export const playClick = () => {
-  const volume = useSound.getState().volumeFX;
-  const clickFX = new Audio(click);
-  clickFX.volume = volume;
-  clickFX.play();
+  const fx = new Audio(InteractionFX.click);
+  playSound(fx);
 }
 
 export const playScribble = () => {
-  const volume = useSound.getState().volumeFX;
-  const scribbleFX = new Audio(scribble);
-  scribbleFX.volume = volume;
-  scribbleFX.play();
+  const fx = new Audio(InteractionFX.scribble);
+  playSound(fx);
 }
 
 export const playSuccess = () => {
-  const volume = useSound.getState().volumeFX;
-  const bubbleFX = new Audio(bubble);
-  bubbleFX.volume = volume;
-  bubbleFX.play();
+  const fx = new Audio(InteractionFX.success);
+  playSound(fx);
 }
 
 export const playVending = () => {
-  const volume = useSound.getState().volumeFX;
-  const vendingFX = new Audio(vending);
-  vendingFX.volume = volume;
-  vendingFX.play();
+  const fx = new Audio(InteractionFX.vend);
+  playSound(fx);
+}
+
+const playSound = (sound: HTMLAudioElement) => {
+  sound.volume = .6 * useSound.getState().volumeFX;
+  sound.play();
 }
