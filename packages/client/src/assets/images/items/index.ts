cf62bcf853b0c5b64890a3776c2c99f4938cf940
aprl <40616911+chlwys@users.noreply.github.com>
2024-08-07 18:24:06 +0100

    item and node world updates (#935)

--- packages/client/src/assets/images/items/index.ts
+++ packages/client/src/assets/images/items/index.ts
@@ -2,9 +2,11 @@ import agency_giftbox from './agency_giftbox.png';
 import astrolabe_disk from './astrolabe_disk.png';
 import best_ice_cream from './best_ice_cream.png';
 import better_ice_cream from './better_ice_cream.png';
+import cheeseburger from './cheeseburger.png';
 import gacha_ticket from './gacha_ticket.png';
 import gakki_cookie_sticks from './gakki_cookie_sticks.png';
 import gaokerena_mochi from './gaokerena_mochi.png';
+import glass_jar from './glass_jar.png';
 import holy_dust from './holy_dust.png';
 import ice_cream from './ice_cream.png';
 import kami_mochi from './kami_mochi.png';
@@ -20,6 +22,7 @@ import red_gakki_ribbon from './red_gakki_ribbon.png';
 import ring_of_spirits from './ring_of_spirits.png';
 import ring_of_water_breathing from './ring_of_water_breathing.png';
 import scrap_metal from './scrap_metal.png';
+import screwdriver from './screwdriver.png';
 import stone from './stone.png';
 import sunset_apple_mochi from './sunset_apple_mochi.png';
 import wooden_stick from './wooden_stick.png';
@@ -32,11 +35,13 @@ export const ItemImages = {
   astrolabe_disk: astrolabe_disk,
   better_ice_cream: better_ice_cream,
   best_ice_cream: best_ice_cream,
+  cheeseburger: cheeseburger,
   gacha_ticket: gacha_ticket,
   gakki_cookie_sticks: gakki_cookie_sticks,
   gaokerena_mochi: gaokerena_mochi,
   holy_dust: holy_dust,
   ice_cream: ice_cream,
+  glass_jar: glass_jar,
   kami_mochi: kami_mochi,
   kamigotchi_world_passport: kamigotchi_world_passport,
   mana_mochi: mana_mochi,
@@ -50,6 +55,7 @@ export const ItemImages = {
   plastic_bottle: plastic_bottle,
   pom_pom_fruit_candy: pom_pom_fruit_candy,
   scrap_metal: scrap_metal,
+  screwdriver: screwdriver,
   stone: stone,
   sunset_apple_mochi: sunset_apple_mochi,
   wooden_stick: wooden_stick,
