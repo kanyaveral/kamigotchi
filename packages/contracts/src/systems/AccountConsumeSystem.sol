751c3f27f19e36f1407dd5bc3c99dd1014b866ee
aprl <40616911+chlwys@users.noreply.github.com>
2024-08-26 15:49:32 +0100

    hardcoded mina teleport scroll (temp) (#998)

--- packages/contracts/src/systems/AccountConsumeSystem.sol
+++ packages/contracts/src/systems/AccountConsumeSystem.sol
@@ -10,6 +10,9 @@ import { LibData } from "libraries/LibData.sol";
 import { LibInventory } from "libraries/LibInventory.sol";
 import { LibItemRegistry } from "libraries/LibItemRegistry.sol";
 
+import { getAddressById, getComponentById } from "solecs/utils.sol";
+import { IndexRoomComponent, ID as RoomCompID } from "components/IndexRoomComponent.sol";
+
 uint256 constant ID = uint256(keccak256("system.Account.Consume"));
 
 // eat one snack
@@ -18,19 +21,23 @@ contract AccountConsumeSystem is System {
 
   function execute(bytes memory arguments) public returns (bytes memory) {
     uint32 itemIndex = abi.decode(arguments, (uint32));
-    uint256 id = LibAccount.getByOperator(components, msg.sender);
+    uint256 accID = LibAccount.getByOperator(components, msg.sender);
 
     // check whether the specified item is consumable
     require(LibItemRegistry.isConsumable(components, itemIndex), "AccountConsume: item not edible");
     require(LibItemRegistry.isForAccount(components, itemIndex), "AccountConsume: beneath you");
 
-    LibAccount.syncStamina(components, id);
-    LibInventory.decFor(components, id, itemIndex, 1); // implicit balance check
-    LibAccount.consume(components, id, itemIndex);
+    LibAccount.syncStamina(components, accID);
+    LibInventory.decFor(components, accID, itemIndex, 1); // implicit balance check
+
+    if (itemIndex == 119) {
+      // hardcode mina teleport scroll - to actually implement soon
+      IndexRoomComponent(getAddressById(components, RoomCompID)).set(accID, 13);
+    } else LibAccount.consume(components, accID, itemIndex);
 
     // standard logging and tracking
-    LibData.inc(components, id, itemIndex, "ITEM_USE", 1);
-    LibAccount.updateLastTs(components, id);
+    LibData.inc(components, accID, itemIndex, "ITEM_USE", 1);
+    LibAccount.updateLastTs(components, accID);
     return "";
   }
 
