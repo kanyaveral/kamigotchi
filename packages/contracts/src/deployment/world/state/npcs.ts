6b9ee4b497d3a096e8c5950a37e1d21e62fab622
aprl <40616911+chlwys@users.noreply.github.com>
2024-08-26 22:07:36 +0100

    incorrect unauthorize writer fix (#1003)

--- packages/contracts/src/deployment/world/state/npcs.ts
+++ packages/contracts/src/deployment/world/state/npcs.ts
@@ -1,6 +1,4 @@
-import { BigNumberish } from 'ethers';
 import { AdminAPI } from '../admin';
-import { getGoalID } from './utils';
 
 export async function initNpcs(api: AdminAPI) {
   await initMerchants(api);
@@ -8,20 +6,7 @@ export async function initNpcs(api: AdminAPI) {
 
 export async function initMerchants(api: AdminAPI) {
   // create our hottie merchant ugajin. names are unique
-  // await api.npc.create(1, 'Mina', 13);
-
-  // // merchant index, item index, buy price, sell price
-  // setListing(api, 1, 101, 50); // gum (S)
-  // setListing(api, 1, 102, 180); // candy (M)
-  // setListing(api, 1, 103, 320); // cookie sticks (L)
-  // setListing(api, 1, 110, 100); // ribbon
-
-  // setListing(api, 1, 107, 160); // ice cream (L)
-  // setListing(api, 1, 108, 90); // ice cream (M)
-  // setListing(api, 1, 109, 50); // ice cream (S)
-
-  // setListing(api, 1, 119, 35); // teleport scroll
-  initRequirement(api, 1, 119, 'COMPLETE_COMP', 'BOOL_IS', 0, getGoalID(5)); // require 1 teleport scroll
+  await api.npc.create(1, 'Mina', 13);
 }
 
 export async function setListing(
@@ -33,15 +18,3 @@ export async function setListing(
 ) {
   await api.listing.set(merchantIndex, itemIndex, buyPrice, sellPrice);
 }
-
-const initRequirement = async (
-  api: AdminAPI,
-  npcIndex: number,
-  itemIndex: number,
-  conditionType: string,
-  logicType: string,
-  index: number,
-  value: BigNumberish
-) => {
-  await api.listing.add.requirement(npcIndex, itemIndex, conditionType, logicType, index, value);
-};
