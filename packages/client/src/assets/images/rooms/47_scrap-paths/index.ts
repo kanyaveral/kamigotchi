2ff43d599febb864300278bf1807ddca9488a6ab
jirach <109483360+JirAcheron@users.noreply.github.com>
2024-10-26 19:13:30 +0100

    add glitch image to scrap paths (#1169)
    stopgap until conditional canvas triggers are up

--- packages/client/src/assets/images/rooms/47_scrap-paths/index.ts
+++ packages/client/src/assets/images/rooms/47_scrap-paths/index.ts
@@ -1,5 +1,5 @@
-import bgPlaytestDay from './backgrounds/playtest-a.png';
-import bgPlaytestEvening from './backgrounds/playtest-b.png';
-import bgPlaytestNight from './backgrounds/playtest-c.png';
+import bgPlaytestDay from './backgrounds/pretest-a.png';
+import bgPlaytestEvening from './backgrounds/pretest-b.png';
+import bgPlaytestNight from './backgrounds/pretest-c.png';
 
 export { bgPlaytestDay, bgPlaytestEvening, bgPlaytestNight };
