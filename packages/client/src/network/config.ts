9231cbeb7a6da93d88e2d886616896e69c632970
CarrotIfson <74294467+CarrotIfson@users.noreply.github.com>
2024-10-31 19:17:48 +0100

    :wrench: Update Rpc urls to new node (#1203)

--- packages/client/src/network/config.ts
+++ packages/client/src/network/config.ts
@@ -91,9 +91,9 @@ function createConfigRawYominet(externalProvider?: ExternalProvider): NetworkCon
   let config: NetworkConfig = <NetworkConfig>{
     devMode: false,
     jsonRpc:
-      'https://maze-jsonrpc-sequencer-9ce4b2ff-e329-459d-8baa-ae49f95f33b2.public-think-factory.newmetric.xyz',
+      'https://maze-jsonrpc-s2-9ce4b2ff-e329-459d-8baa-ae49f95f33b2.public-think-factory.newmetric.xyz',
     wsRpc:
-      'wss://maze-ws-sequencer-9ce4b2ff-e329-459d-8baa-ae49f95f33b2.public-think-factory.newmetric.xyz',
+      'wss://maze-ws-s2-9ce4b2ff-e329-459d-8baa-ae49f95f33b2.public-think-factory.newmetric.xyz',
     snapshotServiceUrl: 'https://kamigaze.test.asphodel.io',
     // faucetServiceUrl: 'https://faucet-lb.test.asphodel.io/',
 
