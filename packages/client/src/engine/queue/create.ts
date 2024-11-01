3c4d598f9c5a59280740eb479d8620af725e6d84
aprl <40616911+chlwys@users.noreply.github.com>
2024-10-31 20:49:49 -0400

    streamline tx.wait() resolver (#1206)
    updates tx.wait() for transaction away from hardcoded 45s poll. This change was originally because one of our previous chains did not play nice with tx.wait().
    
    now waits for 0 block confirmations - slightly faster, no impact (because state is based on events, not tx completition) but faster tx queue processing. Also helps avoid forever pending transactions.

--- packages/client/src/engine/queue/create.ts
+++ packages/client/src/engine/queue/create.ts
@@ -126,7 +126,6 @@ export function create<C extends Contracts>(
         // Populate config and Tx
         const configOverrides = { ...callOverrides, ...txData };
         const populatedTx = await member(...argsWithoutOverrides, configOverrides);
-        // populatedTx.gasLimit = BigNumber.from(populatedTx.gasLimit).mul(2);
         const tx = await target.signer.sendTransaction(populatedTx);
         const hash = tx.hash;
 
@@ -134,15 +133,7 @@ export function create<C extends Contracts>(
           ReturnTypeStrict<(typeof target)[typeof prop]>
         >;
         // This promise is awaited asynchronously in the tx queue and the action queue to catch errors
-        // const wait = async () => (await response).wait();
-        const wait = async () => {
-          const txConfirmed = await target.provider.waitForTransaction(hash, 1, 45000);
-          if (txConfirmed?.status === 0) {
-            // if tx did not complete, initiate tx.wait() to throw regular error
-            return (await response).wait();
-          }
-          return txConfirmed;
-        };
+        const wait = async () => (await response).wait(0);
 
         // Resolved value goes to the initiator of the transaction
         resolve({ hash, wait, response });
@@ -203,13 +194,7 @@ export function create<C extends Contracts>(
         const isRepeatError = error?.reason?.includes('transaction already imported');
         const doResetNonce = isExpirationError || isRepeatError;
 
-        console.log(
-          `[TXQueue] TX Sent\n`,
-          `(error=${!!error},`,
-          `isMutationError=${isMutationError},`,
-          `incNonce=${!!doIncNonce},`,
-          `resetNonce=${!!doResetNonce})`
-        );
+        console.log(`[TXQueue] TX Sent\n`, `Error: ${!!error}\n`);
 
         // Nonce handling
         if (doIncNonce) incNonce();
@@ -221,7 +206,8 @@ export function create<C extends Contracts>(
     // Await confirmation
     if (txResult?.hash) {
       try {
-        await txResult.wait();
+        const tx = await txResult.wait();
+        console.log(`[TXQueue] TX Confirmed\n`, tx);
       } catch (e) {
         console.warn('[TXQueue] tx failed in block', e);
 
