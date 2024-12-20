2e67e3f107de080c0439840b91cac3eacfefd7c3
jirach <109483360+JirAcheron@users.noreply.github.com>
2024-12-20 10:10:03 -0500

    remove old map assets (#1364)
    pretty sure we wont be using these

--- packages/client/src/assets/map/index.ts
+++
@@ -1,6 +0,0 @@
-import { coreSprites } from './map_tiles';
-import { road } from './map_tiles/road';
-import { water } from './map_tiles/water';
-import { rooms } from './rooms';
-
-export { rooms, coreSprites, water, road };
