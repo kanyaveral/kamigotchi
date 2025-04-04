fd00265041730d5a8d43f5197e67cbe9db3c2a05
jirach <109483360+JirAcheron@users.noreply.github.com>
2025-04-04 00:15:51 -0400

    add more environment variables (#1573)
    convert the entire network config into environment variables (sans `provider` override)

--- packages/client/src/network/setup/configs/configs.ts
+++ packages/client/src/network/setup/configs/configs.ts
@@ -1,9 +1,7 @@
 import { ExternalProvider } from '@ethersproject/providers';
+import { Wallet } from 'ethers';
 
 import { SetupContractConfig } from 'network/setup';
-import { createLocal } from './local';
-import { createProduction } from './prod';
-import { createTesting } from './test';
 import { NetworkConfig } from './types';
 
 // Populate the network config based on url params
@@ -12,10 +10,8 @@ export const createConfig = (provider?: ExternalProvider): SetupContractConfig |
 
   // resolve the network config based on the environment mode
   let mode = import.meta.env.MODE;
-  if (mode === 'production') config = createProduction(provider);
-  else if (mode === 'staging') config = createProduction(provider);
-  else if (mode === 'testing') config = createTesting(provider);
-  else config = createLocal(provider);
+  if (mode === 'local') config = getLocalConfig(provider);
+  else config = getConfig(provider);
 
   if (
     config.worldAddress &&
@@ -25,11 +21,52 @@ export const createConfig = (provider?: ExternalProvider): SetupContractConfig |
   ) {
     return shape(config);
   } else {
-    console.error('Invalid network config');
+    console.error('Invalid network config', config);
   }
 };
 
+// get the config of a non-local deployment, populated with environment variables
+export const getConfig = (provider?: ExternalProvider): NetworkConfig => {
+  let config: NetworkConfig = {
+    devMode: false,
+    jsonRpc: import.meta.env.VITE_RPC_TRANSPORT_URL,
+    wsRpc: import.meta.env.VITE_RPC_WS_URL,
+    snapshotServiceUrl: import.meta.env.VITE_KAMIGAZE_URL,
+    streamServiceUrl: import.meta.env.VITE_KAMIGAZE_URL,
+    chainId: Number(import.meta.env.VITE_CHAIN_ID),
+    worldAddress: import.meta.env.VITE_WORLD_ADDRESS,
+    initialBlockNumber: Number(import.meta.env.VITE_INITIAL_BLOCK_NUMBER),
+  };
+
+  // TODO: deprecate second path this whenever it stops being loadbearing
+  if (provider) config.externalProvider = provider;
+  else config.privateKey = Wallet.createRandom().privateKey;
+
+  return config;
+};
+
+// create the network config of a local node deployment
+export const getLocalConfig = (provider?: ExternalProvider): NetworkConfig => {
+  const params = new URLSearchParams(window.location.search);
+  let config: NetworkConfig = <NetworkConfig>{
+    devMode: true,
+    chainId: 1337,
+    jsonRpc: 'http://localhost:8545',
+    wsRpc: 'ws://localhost:8545',
+    worldAddress: params.get('worldAddress') ?? '0xceeDaE2390570eD717A8c07f462D59234b12D39d',
+    initialBlockNumber: parseInt(params.get('initialBlockNumber') ?? '0'),
+  };
+
+  // EOAs and privatekey
+  if (provider) config.externalProvider = provider;
+  else config.privateKey = Wallet.createRandom().privateKey;
+
+  return config;
+};
+
 // shape a flat NetworkConfig struct into SetupContractConfig struct
+// Q: how necessary are all these fields?
+// what if we just forced the inclusion of an external provider?
 const shape: (networkConfig: NetworkConfig) => SetupContractConfig = (config) => ({
   clock: {
     period: 1000, // wtf is this even for
