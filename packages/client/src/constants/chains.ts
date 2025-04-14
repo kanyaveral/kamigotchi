f49a6da8ccfe0a97a18a75ed4323d48d78be217b
aprl <40616911+chlwys@users.noreply.github.com>
2025-04-15 01:47:10 +0800

    remove preyominet (#1603)

--- packages/client/src/constants/chains.ts
+++ packages/client/src/constants/chains.ts
@@ -1,24 +1,7 @@
 import { addRpcUrlOverrideToChain } from '@privy-io/react-auth';
 import { Chain, localhost } from '@wagmi/core/chains';
 
-// chain configuration for testnet
-const TestnetRaw = {
-  id: 4471190363524365,
-  name: 'preyominet',
-  nativeCurrency: {
-    decimals: 18,
-    name: 'Onyx',
-    symbol: 'ONYX',
-  },
-  rpcUrls: {
-    default: { http: [import.meta.env.VITE_RPC_TRANSPORT_URL] },
-  },
-  blockExplorers: {
-    default: { name: 'Onyxscan', url: 'https://scan.testnet.initia.xyz/preyominet-1' },
-  },
-} as const satisfies Chain;
-
-// chain configuration for mainnet
+// chain configuration for mainnet (prod and test world)
 const YominetRaw = {
   id: 428962654539583,
   name: 'yominet',
@@ -36,12 +19,11 @@ const YominetRaw = {
 } as const satisfies Chain;
 
 // TODO: move everything below to the appropriate file
-const testnet = addRpcUrlOverrideToChain(TestnetRaw, import.meta.env.VITE_RPC_TRANSPORT_URL);
 const yominet = addRpcUrlOverrideToChain(YominetRaw, import.meta.env.VITE_RPC_TRANSPORT_URL);
 
 export const chainConfigs: Map<string, Chain> = new Map();
 chainConfigs.set('puter', localhost);
-chainConfigs.set('testing', testnet);
+chainConfigs.set('testing', yominet);
 chainConfigs.set('staging', yominet);
 chainConfigs.set('production', yominet);
 
