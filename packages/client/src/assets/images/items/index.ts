cdda81b8867ac5566d2aa194d96f591861dfc10e
Xenofluxx <93127339+Xenofluxx@users.noreply.github.com>
2025-04-25 17:11:51 +0200

    add  new images (#1656)
    * add items
    
    * change vipp  name
    
    * ♻️ Update kami citizen mediaUri
    
    * ♻️ Update names
    
    ---------
    
    Co-authored-by: hubur <hubur@asphodel.io>

--- packages/client/src/assets/images/items/index.ts
+++ packages/client/src/assets/images/items/index.ts
@@ -1,5 +1,7 @@
 import agency_giftbox from './agency_giftbox.png';
 import astrolabe_disk from './astrolabe_disk.png';
+import astrolabe_part2 from './astrolabe_part2.png';
+import astrolabe_part3 from './astrolabe_part3.png';
 import best_ice_cream from './best_ice_cream.png';
 import better_ice_cream from './better_ice_cream.png';
 import black_poppy from './black_poppy.png';
@@ -8,7 +10,11 @@ import bless_potion from './bless_potion.png';
 import candle from './candle.png';
 import cheeseburger from './cheeseburger.png';
 import daffodil from './daffodil.png';
+import disc_shaped_map from './disc_shaped_map.png';
 import essence_of_daffodil from './essence_of_daffodil.png';
+import eth from './eth.png';
+import field_of_flowers_card_native from './field_of_flowers_card_native.png';
+import flower_bud_card_native from './flower_bud_card_native.png';
 import gacha_ticket from './gacha_ticket.png';
 import gakki_cookie_sticks from './gakki_cookie_sticks.png';
 import gaokerena_mochi from './gaokerena_mochi.png';
@@ -16,11 +22,28 @@ import gingerbread_cookie from './gingerbread_cookie.png';
 import glass_jar from './glass_jar.png';
 import grace_potion from './grace_potion.png';
 import greater_xp_potion from './greater_xp_potion.png';
+import heroic_awakening_card_native from './heroic_awakening_card_native.png';
 import holy_dust from './holy_dust.png';
 import ice_cream from './ice_cream.png';
 import initia_icon from './initia_icon.png';
 import kami_mochi from './kami_mochi.png';
+import kamigotchi_world_citizen_giftbox from './kamigotchi_world_citizen_giftbox.png';
 import kamigotchi_world_passport from './kamigotchi_world_passport.png';
+import kamigotchi_world_passport_1_bit_black_and_white from './kamigotchi_world_passport_1_bit_black_and_white.png';
+import kamigotchi_world_passport_1_bit_green from './kamigotchi_world_passport_1_bit_green.png';
+import kamigotchi_world_passport_1_bit_pink from './kamigotchi_world_passport_1_bit_pink.png';
+import kamigotchi_world_passport_1_bit_red_and_white from './kamigotchi_world_passport_1_bit_red_and_white.png';
+import kamigotchi_world_passport_blue from './kamigotchi_world_passport_blue.png';
+import kamigotchi_world_passport_green from './kamigotchi_world_passport_green.png';
+import kamigotchi_world_passport_grey from './kamigotchi_world_passport_grey.png';
+import kamigotchi_world_passport_lime_green from './kamigotchi_world_passport_lime_green.png';
+import kamigotchi_world_passport_orange from './kamigotchi_world_passport_orange.png';
+import kamigotchi_world_passport_pink from './kamigotchi_world_passport_pink.png';
+import kamigotchi_world_passport_purple from './kamigotchi_world_passport_purple.png';
+import kamigotchi_world_passport_red from './kamigotchi_world_passport_red.png';
+import kamigotchi_world_passport_white from './kamigotchi_world_passport_white.png';
+import lily_sprout_card_native from './lily_sprout_card_native.png';
+import lotus_bud_card_native from './lotus_bud_card_native.png';
 import mana_mochi from './mana_mochi.png';
 import maple_flavor_ghost_gum from './maple_flavor_ghost_gum.png';
 import microplastics from './microplastics.png';
@@ -41,12 +64,14 @@ import resin from './resin.png';
 import respec_potion from './respec_potion.png';
 import ring_of_spirits from './ring_of_spirits.png';
 import ring_of_water_breathing from './ring_of_water_breathing.png';
+import river_of_life_card_native from './river_of_life_card_native.png';
 import scrap_metal from './scrap_metal.png';
 import screwdriver from './screwdriver.png';
 import scroll_of_shop_teleportation from './scroll_of_shop_teleportation.png';
 import shredded_mint from './shredded_mint.png';
 import spice_grinder from './spice_grinder.png';
 import stone from './stone.png';
+import strange_machine_part_native from './strange_machine_part_native.png';
 import sunset_apple_mochi from './sunset_apple_mochi.png';
 import vipp from './vipp.png';
 import wooden_stick from './wooden_stick.png';
@@ -57,6 +82,32 @@ import xp_candy_small from './xp_candy_small.png';
 import xp_potion from './xp_potion.png';
 
 export const ItemImages = {
+  river_of_life_card_native: river_of_life_card_native,
+  strange_machine_part_native: strange_machine_part_native,
+  vipp: vipp,
+  kamigotchi_world_passport_white: kamigotchi_world_passport_white,
+  kamigotchi_world_passport_orange: kamigotchi_world_passport_orange,
+  kamigotchi_world_passport_pink: kamigotchi_world_passport_pink,
+  kamigotchi_world_passport_purple: kamigotchi_world_passport_purple,
+  kamigotchi_world_passport_red: kamigotchi_world_passport_red,
+  kamigotchi_world_passport_lime_green: kamigotchi_world_passport_lime_green,
+  kamigotchi_world_citizen_giftbox: kamigotchi_world_citizen_giftbox,
+  kamigotchi_world_passport_grey: kamigotchi_world_passport_grey,
+  kamigotchi_world_passport_green: kamigotchi_world_passport_green,
+  kamigotchi_world_passport_blue: kamigotchi_world_passport_blue,
+  kamigotchi_world_passport_1_bit_red_and_white: kamigotchi_world_passport_1_bit_red_and_white,
+  kamigotchi_world_passport_1_bit_pink: kamigotchi_world_passport_1_bit_pink,
+  kamigotchi_world_passport_1_bit_green: kamigotchi_world_passport_1_bit_green,
+  kamigotchi_world_passport_1_bit_black_and_white: kamigotchi_world_passport_1_bit_black_and_white,
+  lotus_bud_card_native: lotus_bud_card_native,
+  lily_sprout_card_native: lily_sprout_card_native,
+  heroic_awakening_card_native: heroic_awakening_card_native,
+  flower_bud_card_native: flower_bud_card_native,
+  field_of_flowers_card_native: field_of_flowers_card_native,
+  eth: eth,
+  disc_shaped_map: disc_shaped_map,
+  astrolabe_part2: astrolabe_part2,
+  astrolabe_part3: astrolabe_part3,
   init: initia_icon,
   giftbox: agency_giftbox,
   astrolabe_disk: astrolabe_disk,
