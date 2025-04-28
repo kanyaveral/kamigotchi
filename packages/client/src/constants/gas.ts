b3a7b4b7625f3fc8829bba668aebe71ba28310f1
aprl <40616911+chlwys@users.noreply.github.com>
2025-04-28 20:08:13 +0800

    update gas constants (#1672)

--- packages/client/src/constants/gas.ts
+++ packages/client/src/constants/gas.ts
@@ -1,13 +1,13 @@
 // TODO: fix gas wei conversion
-const txCostEst = 5 / 10e4;
+const txCostEst = 1 / 10e4;
 
 export const GasConstants = {
-  Max: 1000 * txCostEst,
-  Full: 100 * txCostEst,
-  Half: 50 * txCostEst,
-  Quarter: 25 * txCostEst,
-  Low: 10 * txCostEst,
-  Warning: 5 * txCostEst,
+  Max: 1500 * txCostEst,
+  Full: 1000 * txCostEst,
+  Half: 500 * txCostEst,
+  Quarter: 250 * txCostEst,
+  Low: 100 * txCostEst,
+  Warning: 10 * txCostEst,
   Empty: 0,
 };
 
