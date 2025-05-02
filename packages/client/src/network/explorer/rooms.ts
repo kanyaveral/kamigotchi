69ced465baf7cfd47e007c0f6bcf250e5bc64582
aprl <40616911+chlwys@users.noreply.github.com>
2025-05-02 16:19:11 +0800

    explorer room players (#1706)

--- packages/client/src/network/explorer/rooms.ts
+++ packages/client/src/network/explorer/rooms.ts
@@ -1,6 +1,7 @@
 import { EntityIndex, World } from '@mud-classic/recs';
 
 import { Components } from 'network/';
+import { getAccount, queryRoomAccounts } from 'network/shapes/Account';
 import { getAllRooms, getRoom, getRoomByIndex } from 'network/shapes/Room';
 
 export const rooms = (world: World, components: Components) => {
@@ -8,6 +9,8 @@ export const rooms = (world: World, components: Components) => {
     all: () => getAllRooms(world, components),
     get: (entity: EntityIndex) => getRoom(world, components, entity),
     getByIndex: (index: number) => getRoomByIndex(world, components, index),
+    getPlayers: (index: number) =>
+      queryRoomAccounts(components, index).map((entity) => getAccount(world, components, entity)),
     indices: () => Array.from(components.RoomIndex.values.value.values()),
   };
 };
