19e7d68ccabeddf23cf075e5fe68f2cb0428945f
jirach <109483360+JirAcheron@users.noreply.github.com>
2025-05-04 04:52:45 -0400

    link onyx menu button to pool (#1740)
    for our bags

--- packages/client/src/app/components/fixtures/menu/buttons/Presale.tsx
+++ packages/client/src/app/components/fixtures/menu/buttons/Presale.tsx
@@ -1,37 +1,35 @@
-import { Modals } from 'app/stores';
+import { IconButton, Tooltip } from 'app/components/library';
+import { useTokens } from 'app/stores';
 import { ItemImages } from 'assets/images/items';
-import { MenuButton } from './MenuButton';
 
-interface Props {
-  disabled?: boolean;
-}
+const ONYX_ADDR = '0x4BaDFb501Ab304fF11217C44702bb9E9732E7CF4';
 
-export const PresaleMenuButton = (props: Props) => {
-  const { disabled } = props;
+export const PresaleMenuButton = () => {
+  const { balances } = useTokens();
 
-  const modalsToHide: Partial<Modals> = {
-    chat: false,
-    crafting: false,
-    bridgeERC20: false,
-    bridgeERC721: false,
-    dialogue: false,
-    emaBoard: false,
-    gacha: false,
-    goal: false,
-    kami: false,
-    nameKami: false,
-    node: false,
-    quests: false,
+  const onyxInfo = balances.get(ONYX_ADDR);
+  const balance = onyxInfo?.balance ?? 0;
+  const allowance = onyxInfo?.allowance ?? 0;
+
+  const openBaselineLink = () => {
+    window.open(`https://app.baseline.markets/trade/yominet/${ONYX_ADDR}`, '_blank');
   };
 
   return (
-    <MenuButton
-      id='presale_button'
-      image={ItemImages.onyx}
-      tooltip={disabled ? 'Disabled' : 'Presale'}
-      targetModal='presale'
-      hideModals={modalsToHide}
-      disabled={disabled}
-    />
+    <Tooltip
+      text={[
+        'Acquire more $ONYX',
+        `Balance: ${balance.toFixed(3)} `,
+        `Allowance: ${allowance.toFixed(3)}`,
+      ]}
+    >
+      <IconButton
+        img={ItemImages.onyx}
+        text={balance?.toFixed(3)}
+        onClick={openBaselineLink}
+        scale={4.5}
+        scaleOrientation='vh'
+      />
+    </Tooltip>
   );
 };
