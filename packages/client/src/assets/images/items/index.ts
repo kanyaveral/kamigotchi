2f651e45ea43844946e01b1208eeebcaeb871f61
Acheron <109483360+JirAcheron@users.noreply.github.com>
2025-07-28 03:32:56 -0400

    remove unused item assets (#1935)

--- packages/client/src/assets/images/items/index.ts
+++ packages/client/src/assets/images/items/index.ts
@@ -9,7 +9,6 @@ import better_ice_cream from './better_ice_cream.png';
 import black_poppy from './black_poppy.png';
 import black_poppy_extract from './black_poppy_extract.png';
 import bless_potion from './bless_potion.png';
-import candle from './candle.png';
 import chalkberry from './chalkberry.png';
 import cheeseburger from './cheeseburger.png';
 import cultivation_i_spell_card from './cultivation_i_spell_card.png';
@@ -24,8 +23,6 @@ import eth from './eth.png';
 import festival_chime from './festival_chime.png';
 import gacha_ticket from './gacha_ticket.png';
 import gakki_cookie_sticks from './gakki_cookie_sticks.png';
-import gaokerena_mochi from './gaokerena_mochi.png';
-import gingerbread_cookie from './gingerbread_cookie.png';
 import glass_jar from './glass_jar.png';
 import grace_potion from './grace_potion.png';
 import greater_xp_potion from './greater_xp_potion.png';
@@ -34,7 +31,6 @@ import holy_syrup from './holy_syrup.png';
 import hostility_potion from './hostility_potion.png';
 import ice_cream from './ice_cream.png';
 import initia_icon from './initia_icon.png';
-import kami_mochi from './kami_mochi.png';
 import kamigotchi_world_citizen_giftbox from './kamigotchi_world_citizen_giftbox.png';
 import kamigotchi_world_passport from './kamigotchi_world_passport.png';
 import kamigotchi_world_passport_1_bit_black_and_white from './kamigotchi_world_passport_1_bit_black_and_white.png';
@@ -51,12 +47,10 @@ import kamigotchi_world_passport_purple from './kamigotchi_world_passport_purple
 import kamigotchi_world_passport_red from './kamigotchi_world_passport_red.png';
 import kamigotchi_world_passport_white from './kamigotchi_world_passport_white.png';
 import kamigotchi_world_passport_yellow from './kamigotchi_world_passport_yellow.png';
-import mana_mochi from './mana_mochi.png';
 import maple_flavor_ghost_gum from './maple_flavor_ghost_gum.png';
 import melkarths_heroic_awakening_spell_card from './melkarths_heroic_awakening_spell_card.png';
 import microplastics from './microplastics.png';
 import mint from './mint.png';
-import mistletoe from './mistletoe.png';
 import musu from './musu.png';
 import musu_magnet from './musu_magnet.png';
 import neiths_river_of_life_spell_card from './neiths_river_of_life_spell_card.png';
@@ -66,7 +60,6 @@ import pine_cone from './pine_cone.png';
 import pine_pollen from './pine_pollen.png';
 import plastic_bottle from './plastic_bottle.png';
 import pom_pom_fruit_candy from './pom_pom_fruit_candy.png';
-import pomegranate from './pomegranate.png';
 import portable_burner from './portable_burner.png';
 import powdered_red_amber from './powdered_red_amber.png';
 import red_amber_crystal from './red_amber_crystal.png';
@@ -75,23 +68,15 @@ import reroll_ticket from './reroll_ticket.png';
 import resin from './resin.png';
 import resin_tincture from './resin_tincture.png';
 import respec_potion from './respec_potion.png';
-import ring_of_spirits from './ring_of_spirits.png';
-import ring_of_water_breathing from './ring_of_water_breathing.png';
 import sanguine_shroom from './sanguine_shroom.png';
 import sanguineous_powder from './sanguineous_powder.png';
 import scrap_metal from './scrap_metal.png';
 import screwdriver from './screwdriver.png';
-import scroll_of_shop_teleportation from './scroll_of_shop_teleportation.png';
 import shredded_mint from './shredded_mint.png';
 import spice_grinder from './spice_grinder.png';
 import stone from './stone.png';
-import sunset_apple_mochi from './sunset_apple_mochi.png';
 import vipp from './vipp.png';
 import wooden_stick from './wooden_stick.png';
-import xp_candy_huge from './xp_candy_huge.png';
-import xp_candy_large from './xp_candy_large.png';
-import xp_candy_medium from './xp_candy_medium.png';
-import xp_candy_small from './xp_candy_small.png';
 import xp_potion from './xp_potion.png';
 
 export const ItemImages = {
@@ -116,7 +101,6 @@ export const ItemImages = {
   black_poppy_extract: black_poppy_extract,
   black_poppy: black_poppy,
   bless_potion: bless_potion,
-  candle: candle,
   cheeseburger: cheeseburger,
   cultivation_i_spell_card: cultivation_i_spell_card,
   cultivation_ii_spell_card: cultivation_ii_spell_card,
@@ -127,16 +111,13 @@ export const ItemImages = {
   eth: eth,
   gacha_ticket: gacha_ticket,
   gakki_cookie_sticks: gakki_cookie_sticks,
-  gaokerena_mochi: gaokerena_mochi,
   giftbox: agency_giftbox,
-  gingerbread_cookie: gingerbread_cookie,
   glass_jar: glass_jar,
   grace_potion: grace_potion,
   greater_xp_potion: greater_xp_potion,
   holy_dust: holy_dust,
   ice_cream: ice_cream,
   init: initia_icon,
-  kami_mochi: kami_mochi,
   kamigotchi_world_citizen_giftbox: kamigotchi_world_citizen_giftbox,
   kamigotchi_world_passport_1_bit_black_and_white: kamigotchi_world_passport_1_bit_black_and_white,
   kamigotchi_world_passport_1_bit_green: kamigotchi_world_passport_1_bit_green,
@@ -153,12 +134,10 @@ export const ItemImages = {
   kamigotchi_world_passport_white: kamigotchi_world_passport_white,
   kamigotchi_world_passport_yellow: kamigotchi_world_passport_yellow,
   kamigotchi_world_passport: kamigotchi_world_passport,
-  mana_mochi: mana_mochi,
   maple_flavor_ghost_gum: maple_flavor_ghost_gum,
   melkarths_heroic_awakening_spell_card: melkarths_heroic_awakening_spell_card,
   microplastics: microplastics,
   mint: mint,
-  mistletoe: mistletoe,
   musu: musu,
   neiths_river_of_life_spell_card: neiths_river_of_life_spell_card,
   onyx: onyx,
@@ -167,27 +146,18 @@ export const ItemImages = {
   pine_pollen: pine_pollen,
   plastic_bottle: plastic_bottle,
   pom_pom_fruit_candy: pom_pom_fruit_candy,
-  pomegranate: pomegranate,
   portable_burner: portable_burner,
   red_amber_crystal: red_amber_crystal,
   red_gakki_ribbon: red_gakki_ribbon,
   reroll_ticket: reroll_ticket,
   resin: resin,
   respec_potion: respec_potion,
-  ring_of_spirits: ring_of_spirits,
-  ring_of_water_breathing: ring_of_water_breathing,
   scrap_metal: scrap_metal,
   screwdriver: screwdriver,
-  scroll_of_shop_teleportation: scroll_of_shop_teleportation,
   shredded_mint: shredded_mint,
   spice_grinder: spice_grinder,
   stone: stone,
-  sunset_apple_mochi: sunset_apple_mochi,
   vipp: vipp,
   wooden_stick: wooden_stick,
-  xp_candy_huge: xp_candy_huge,
-  xp_candy_large: xp_candy_large,
-  xp_candy_medium: xp_candy_medium,
-  xp_candy_small: xp_candy_small,
   xp_potion: xp_potion,
 };
